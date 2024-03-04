825589f315bb056e42c3d46cac88911ba8ffb55f
MikkoLyly <mikko.lyly@outlook.com>
2024-03-04 17:30:23 +0200

    Updated board config

--- boards/simo/board_config.h
+++ boards/simo/board_config.h
@@ -2,10 +2,10 @@
 #define BOARD_CONFIG_H
 
 #define VENDOR_NAME "Microchip Technology, Inc."
-#define PRODUCT_NAME "SIMO USB RS422"
+#define PRODUCT_NAME "SIMO Mainboard"
 #define VOLUME_LABEL "SIMO"
 
-#define BOARD_ID "SAMD21G18A-SIMO-v0"
+#define BOARD_ID "SAMD21G18A-SIMO-MAIN-v0"
 
 #define USB_VID 0x04D8
 #define USB_PID 0xE5EE
@@ -13,7 +13,7 @@
 #define INDEX_URL "https://en.wikipedia.org/wiki/RS-422"
 
 #define LED_PIN PIN_PA17
-#define LED_TX_PIN PIN_PA27 // NC
-#define LED_RX_PIN PIN_PB03 // NC
+// #define LED_TX_PIN PIN_PA27 // NC
+// #define LED_RX_PIN PIN_PB03 // NC
 
 #endif
